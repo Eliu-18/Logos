596f43ca5ff53e0a433aa55d01cc94a7df3acd2e
unknown <eliubernab@gmail.com>
2023-08-31 00:26:15 -0500

    Modifiqué la app del clima

--- src/Paginas/PublicidadPagina.js
+++ src/Paginas/PublicidadPagina.js
@@ -3,13 +3,18 @@ import React from 'react';
 export default class WeatherApp extends React.Component {
   state = {
     weatherData: null,
+    cityInput: '',
   };
 
-  componentDidMount() {
-    const apiKey = '46ef44ed62154f98d91a333a6845fec9'; // Reemplaza con tu clave de API de OpenWeatherMap
-    const city = 'California'; // Cambia a la ciudad para la que deseas obtener el pronóstico
+  handleInputChange = event => {
+    this.setState({ cityInput: event.target.value });
+  };
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+  fetchWeatherData = () => {
+    const apiKey = '46ef44ed62154f98d91a333a6845fec9'; 
+    const { cityInput } = this.state;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`)
       .then(response => response.json())
       .then(data => {
         this.setState({ weatherData: data });
@@ -17,25 +22,50 @@ export default class WeatherApp extends React.Component {
       .catch(error => {
         console.error('Error fetching weather data:', error);
       });
-  }
+  };
 
   render() {
-    const { weatherData } = this.state;
+    const { weatherData, cityInput } = this.state;
 
     return (
       <div>
-        <h1>Weather App</h1>
+        <h1 style={{color:'#cb3636'}}>App del Clima</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Ingresa la ciudad"
+            value={cityInput}
+            onChange={this.handleInputChange}
+            style={{
+                backgroundColor: 'white',
+                color: 'black',
+                border: 'none',
+                padding: '10px 20px',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                margin: "20px"
+              }}
+          />
+          <button style={{
+              backgroundColor: '#007bff',
+              color: 'white',
+              border: 'none',
+              padding: '10px 20px',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }} onClick={this.fetchWeatherData}>Obtener el clima</button>
+        </div>
         {weatherData && (
           <div>
-            <h2>{weatherData.name}</h2>
+            <h2 style={{color:'#cb3636'}}>{weatherData.name}</h2>
             {weatherData.sys && weatherData.sys.country && (
-              <p>Country: {weatherData.sys.country}</p>
+              <p><b style={{color:'white'}}>País:</b> {weatherData.sys.country}</p>
             )}
             {weatherData.main && (
-              <p>Temperature: {weatherData.main.temp}°C</p>
+              <p><b style={{color:'white'}}>Temperatura:</b> {weatherData.main.temp}°C</p>
             )}
             {weatherData.weather && weatherData.weather[0] && (
-              <p>Weather: {weatherData.weather[0].description}</p>
+              <p><b style={{color:'white'}}>Clima:</b> {weatherData.weather[0].description}</p>
             )}
           </div>
         )}
